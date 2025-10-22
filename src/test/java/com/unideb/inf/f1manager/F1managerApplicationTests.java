30f1a7e2d3215755e4e486ba15e91ec180cf6919
Kótai Erik Ralf <erikkotai@gmail.com>
2025-10-22 19:01:00 +0200

    projekt init

---
+++ src/test/java/com/unideb/inf/f1manager/F1managerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unideb.inf.f1manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1managerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
